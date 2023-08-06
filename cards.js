af74166abefffa018e202c69b28dbf414cef8157
danielkieuu <2717881k@student.gla.ac.uk>
2023-08-06 23:09:02 +0100

    Update teamSite
    Removed unnecessary files

--- cards.js
+++
@@ -1,7 +0,0 @@
-var cards = document.querySelectorAll(".cardnewer");
-
-[...cards].forEach((card) => {
-  card.addEventListener("click", function () {
-    card.classList.toggle("is-flipped");
-  });
-});
